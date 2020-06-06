6bf42c36077013f3ba0a97867d559a4daecead92
kromkazpasztetem <reloaded.email@gmail.com>
2020-06-06 21:57:58 +0200

    Optimized procedure randwalk

--- randwalk.c
+++ randwalk.c
@@ -55,6 +55,35 @@ int randwalk0(uint8_t *arr, int n, int len) {
 }
 
 int randwalk1(uint8_t *arr, int n, int len) {
-  /* TODO: Fill in this procedure! */
-  return 0;
+  int sum = 0;
+  int i, j, k = 0;
+  uint64_t dir = 0;
+
+  // Start in the center of 2D array
+  i = n / 2, j = n / 2;
+
+  do {
+    k -= 2;
+    if (k < 0) {
+      k = 62;
+      dir = fast_random();
+    }
+
+    int d = (dir >> k) & 3;
+
+    sum += arr[i * n + j];
+    
+    
+    int i2 = (i > 0) & (d == 0);
+    int i3 = (i < n-1) & (d == 1);
+        
+    int j2 = (j > 0) & (d == 2);
+    int j3 = (j < n-1) & (d == 3);
+    
+    i = i - i2 + i3;
+    j = j - j2 + j3;
+
+  } while (--len);
+  
+  return sum;
 }
